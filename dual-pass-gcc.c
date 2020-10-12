e769b4c345c7bf02ac98e5a0bb877f5f33970476
FrozenVoid <FrozenVoid@users.noreply.github.com>
2020-10-12 21:48:20 +0300

    Create dual-pass-gcc.c

---
+++ dual-pass-gcc.c
@@ -0,0 +1,9 @@
+//dual pass example:
+//compile: gcc code.c -E|gcc -xc -
+#include "print.h"
+#include "arguments.h"
+#define D #define
+#define DEFMACRO(name,tup,body...) D name(id tup) body
+DEFMACRO(quack,(a,b,c),a+b+c)
+int x=quack(1,2,3);
+print(x);//6
