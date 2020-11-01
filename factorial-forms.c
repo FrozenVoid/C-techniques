789735e78c561f52afc11e3bc947a81393937f5c
FrozenVoid <FrozenVoid@users.noreply.github.com>
2020-11-01 13:37:28 +0000

    Update factorial-forms.c

--- factorial-forms.c
+++ factorial-forms.c
@@ -8,12 +8,18 @@ return n == 0 ? 1 : n * elegantfactorial(n - 1);
 uint64_t uglyfactorial(uint64_t n) {
 uint64_t i=1; while(n)i*=n--; return i;
 }
-
+// tail call:recursion without stack cost
+void tailcall(uint64_t* result,uint64_t n){
+if(n==0)return;
+*result=(*result) *n;
+return nonrec(result,n-1);
+}
 
 int main(int argc,char**argv){
 uint64_t x=atoll(argv[1]);
 uint64_t a=elegantfactorial(x);
 uint64_t b=uglyfactorial(x);
-print(a,b);
+uint64_t c=1;tailcall(&c,x);
+print(a,b,c);
 
 }
