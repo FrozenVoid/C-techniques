df26a2acfd757916fa7d11d3ad2ea6241a950691
FrozenVoid <FrozenVoid@users.noreply.github.com>
2021-02-11 02:57:48 +0200

    Update rndstring.c

--- rng/rndstring.c
+++ rng/rndstring.c
@@ -1,15 +1,15 @@
 #include "Util/void.h"
-//random valid ASCII string with default len=5
 char rndchar(uint64_t a){
 const char * valid=" !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
 return valid[a%95];
 ;}
 
+
 int main(int argc,char**argv){
 uint64_t rndttime=__rdtsc();
 randomize(rndttime);
-int maxlen=5;
-if(argc>1)maxlen=atoi(argv[1]);
+uint16_t maxlen=5;
+if(argc>1)maxlen=(uint16_t)atoi(argv[1]);
 char res[maxlen+1];res[maxlen]=0;
 for(int i=0;i<maxlen;i++){randomize(rndttime);
 res[i]=rndchar(rndttime);}
@@ -17,3 +17,4 @@ print(res);
 
 return 0;
 }
+
