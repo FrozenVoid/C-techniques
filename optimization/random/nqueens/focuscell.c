4a227c11946097d26305e4c348591db543c96ba0
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-02-10 01:34:02 +0200

    Update focuscell.c

--- optimization/random/nqueens/focuscell.c
+++ optimization/random/nqueens/focuscell.c
@@ -2,7 +2,6 @@
 
 //fast N Queens "focus cell" solver
 
-//faster with QDEBUG==1
 #define QDEBUG 1//print intersect count each INTERSECT_DISP seconds
 #define INTERSECT_DISP 1//1 seconds
 #define MS_CLOCK (CLOCKS_PER_SEC/1000)
@@ -15,13 +14,45 @@ for(size_t i=0;i<len;i++){int cur=board[i],zc=0;
  if(((z-i)==tabs(zqueen-cur))){last=z;zc++;sum++; };   }  }
 return sum;}
 
+uint64_t log2index(size_t X){return ((unsigned) (63 - __builtin_clzll((X)) ))      ;}
+size_t firstq(int*board,size_t len){//first intersect
+size_t i,z;
+for( i=0;i<len;i++){int cur=board[i];
+ for( z=i+1;z<len;z++){int zqueen=board[z];
+ if(((z-i)==tabs(zqueen-cur))){return z; };   }  }
+return len;}
+
+//presolve positions
+void psolve(int* q,int N){int temp;
+#define swapq(x,y) temp=x;x=y;y=temp;
+u64 limlq=128/log2index(N);
+u64 fq=firstq(q,N),best=fq,cur,lastq=0, A,B;
+while(fq<(N-2) && lastq<limlq){
+B=fq;//force focus cell to swap queens
+do{A=randuint64()%N;}while(!(B^A));
+swapq(q[A],q[B]);
+cur=firstq(q,N);//count diagonal intersects
+if(cur<B){lastq++; //revert if worse
+swapq(q[A],q[B]);continue;  }
+fq=firstq(q,N);lastq=0;
+//new record
+#if QDEBUG
+clock_t curt=clock();
+  if(curt-startt2> INTERSECT_DISP*CLOCKS_PER_SEC){
+  print("Time(s):",(curt-startt)/CLOCKS_PER_SEC,"Complete:",B,"/",N,"\n");startt2=curt;}
+#else
+fflush(stdout);//fix low priority;
+#endif
+
+} //end loop
+}
 
 
 
 void solve(int* q,int N){int temp;
 #define swapq(x,y) temp=x;x=y;y=temp;
 
-
+if(N>64)psolve(q,N);
 u64 cur=diags(q,N),best=cur, A,B;
 while(cur){
 B=last;//force focus cell to swap queens
@@ -30,23 +61,27 @@ swapq(q[A],q[B]);
 cur=diags(q,N);//count diagonal intersects
 if(cur>best){ //revert if worse
 swapq(q[A],q[B]);continue;  }
-best=cur;//new record
+best=cur;
+//new record
 #if QDEBUG
 clock_t curt=clock();
   if(curt-startt2> INTERSECT_DISP*CLOCKS_PER_SEC){
   print("Time(s):",(curt-startt)/CLOCKS_PER_SEC,"Intersections:",cur,"\n");startt2=curt;}
+#else
+fflush(stdout);//fix low priority;
 #endif
 
 } //end loop
 }
 
-int main(int argc,char**argv){startt=clock();startt2=clock();
+
+int main(int argc,char**argv){startt=clock();startt2=startt;
 if(argc!=2){syntax:;puts("Syntax:nq N\n N=Board size(4+)");exit(1);}
 int N=atoi(argv[1]);if(N<4)goto syntax;
 int* q=malloc(sizeof(int)*N);//queen row/cols(2^31-1 max)
 if(!q){perror("Queen array size too large");exit(2);}
 for(int i=0;i<N;i++)q[i]=i;//unique rows/cols to swap.
-solve(q,N);
+;solve(q,N);
 for(int i=0;i<N;i++)printf("%d,",q[i]+1);
 print("\nSolution found in:",(clock()-startt)/MS_CLOCK," milliseconds\n");
 
