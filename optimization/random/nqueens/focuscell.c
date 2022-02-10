0bae2fa2a7524bf94c4764e0bda0613d116fe763
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-02-10 04:34:51 +0200

    Update focuscell.c

--- optimization/random/nqueens/focuscell.c
+++ optimization/random/nqueens/focuscell.c
@@ -5,13 +5,13 @@
 #define QDEBUG 1//print intersect count each INTERSECT_DISP seconds
 #define INTERSECT_DISP 1//1 seconds
 #define MS_CLOCK (CLOCKS_PER_SEC/1000)
-size_t last=0;//focus column(last intersect)
+size_t last=0,fst=0;//focus column(last intersect)
 clock_t startt,startt2;
 size_t diags(int*board,size_t len){//optimization metric
 size_t sum=0;
 for(size_t i=0;i<len;i++){int cur=board[i],zc=0;
  for(size_t z=i+1;z<len&&zc<2;z++){int zqueen=board[z];
- if(((z-i)==tabs(zqueen-cur))){last=z;zc++;sum++; };   }  }
+ if(((z-i)==tabs(zqueen-cur))){fst=i;last=z;zc++;sum++; };   }  }
 return sum;}
 
 uint64_t log2index(size_t X){return ((unsigned) (63 - __builtin_clzll((X)) ))      ;}
@@ -21,30 +21,49 @@ for( i=0;i<len;i++){int cur=board[i];
  for( z=i+1;z<len;z++){int zqueen=board[z];
  if(((z-i)==tabs(zqueen-cur))){return z; };   }  }
 return len;}
+size_t lstq(int*board,size_t len){//first intersect
+size_t i,z;
+for( i=len-1;i>1;i--){int cur=board[i];
+ for( z=i-1;z>2;z--){int zqueen=board[z];
+ if(((z-i)==tabs(zqueen-cur))){fst=i;last=z;return z; };   }  }
+return 0;}
+size_t firstq1(int*board,size_t len){//first intersect
+size_t i,z;
+for( i=0;i<len;i++){int cur=board[i];
+ for( z=i+1;z<len;z++){int zqueen=board[z];
+ if(((z-i)==tabs(zqueen-cur))){fst=i;last=z;return i; };   }  }
+return len;}
 
 //presolve positions
 void psolve(int* q,int N){int temp;
 #define swapq(x,y) temp=x;x=y;y=temp;
-u64 limlq=128/log2index(N);
-u64 fq=firstq(q,N),best=fq,cur,lastq=0, A,B;
-while(fq<(N-2) && lastq<limlq){
-B=fq;//force focus cell to swap queens
-do{A=randuint64()%N;}while(!(B^A));
+u64 limlq=1280/log2index(N);
+u64 cur,lastq=0, A,B;
+while(firstq1(q,N)<(N-2) && lastq<limlq){
+B=randuint64()&1?last:fst;//force focus cell to swap queens
+
+do{A=randuint64()%N;
+}while(!(B^A) );
 swapq(q[A],q[B]);
-cur=firstq(q,N);//count diagonal intersects
-if(cur<B){lastq++; //revert if worse
-swapq(q[A],q[B]);continue;  }
-fq=firstq(q,N);lastq=0;
+
+cur=fst;
+;//count diagonal intersects
+if(firstq1(q,N)<cur){lastq++; //revert if worse
+swapq(q[A],q[B]);
+continue;  }
+lastq=0;
 //new record
 #if QDEBUG
 clock_t curt=clock();
   if(curt-startt2> INTERSECT_DISP*CLOCKS_PER_SEC){
-  print("Time(s):",(curt-startt)/CLOCKS_PER_SEC,"Complete:",B,"/",N,"\n");startt2=curt;}
+  print("Time(s):",(curt-startt)/CLOCKS_PER_SEC,"Complete:",fst,"/",N,"\n");startt2=curt;}
 #else
 fflush(stdout);//fix low priority;
 #endif
 
 } //end loop
+
+
 }
 
 
@@ -52,11 +71,13 @@ fflush(stdout);//fix low priority;
 void solve(int* q,int N){int temp;
 #define swapq(x,y) temp=x;x=y;y=temp;
 
-if(N>64)psolve(q,N);
+psolve(q,N);
 u64 cur=diags(q,N),best=cur, A,B;
 while(cur){
-B=last;//force focus cell to swap queens
+B=randuint64()&1?last:fst;
 do{A=randuint64()%N;}while(!(B^A));
+
+
 swapq(q[A],q[B]);
 cur=diags(q,N);//count diagonal intersects
 if(cur>best){ //revert if worse
