ea140663320e1fbfd6bf3ab2536241a63f387616
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-02-15 15:46:24 +0200

    Update linear.c

--- optimization/random/nqueens/linear.c
+++ optimization/random/nqueens/linear.c
@@ -1,22 +1,38 @@
 #include "Util/void.h"//https://github.com/FrozenVoid/C-headers
 //linear ~O(N) NQueens  solver
+
 #define NCYCLES 8 //report each NCYCLES
 #define mstime() ((clock())/(CLOCKS_PER_SEC/1000))
 #define tsctime(c) ((__rdtsc()-c)>>30)
 #define SCRAMBLE 8//scramble iters 0-N
 #define QDEBUG 1//print debug/iteration data
 #define NONLINEAR 1//ending search if (cols<log(N))
-size_t N;
-int * board;
-int * diagL;i64 sumL=0;
-int * diagR;i64 sumR=0;
-i64 swapt=0,swaps=0;//valid swaps total(set if QDEBUG enabled)
-size_t loops=0,fail=0,tfail=0,dir=1,tswaps=0,cend,A,B,valr,cur,best;
-uint64_t log2index(size_t X){return ((unsigned) (63 - __builtin_clzll((X)) ))      ;}
-#define swapq(x,y) ({int temp=x;x=y;y=temp;})
+#define val_t u32
+#define rotate(num,bits) ({ typeof(num) x=num;\
+x=(x>>bits)|(x<<((sizeof(x)*8)-bits));x;})
+#define rndgen64 randuint64
+#define rndgen32 msws
 
- void swapc(size_t x,size_t y){
-i64  clx,crx,cly,cry;
+//from https://arxiv.org/abs/1704.00358
+static uint32_t msws() {
+static uint64_t x = 0, w = 0, s = 0xb5ad4eceda1ce2a9;
+   x *= x; x += (w += s); return x = (x>>32) | (x<<32);}
+static u64 tseedw=0;
+
+
+
+
+val_t N,A,B;
+val_t * board;
+val_t * diagL;i64 sumL=0;
+val_t * diagR;i64 sumR=0;
+i64 swapt=0,swaps=0;//valid swaps total(set if QDEBUG enabled)
+size_t loops=0,fail=0,tfail=0,dir=1,tswaps=0,cend,valr,cur,best;
+uint64_t log2index(size_t X){return ((unsigned) (63 - __builtin_clzll((X)) ))      ;}
+#define swapq(x,y) ({val_t temp=x;x=y;y=temp;})
+
+ void swapc(val_t x,val_t y){
+val_t  clx,crx,cly,cry;
 #if QDEBUG
 tswaps++;swaps+=dir;//valid swaps total
 #endif
@@ -47,24 +63,27 @@ sumR+=!!(cry-1);
 
 
 //queen collisons at position: 2=none,2+=collision
- i32 qccount(size_t P){
+ val_t qccount(u32 P){
 //cannot be zero due being set from q[]
-int s=board[P];
-return ((diagL[s+P]))+((diagR[s+(N-P)]))-2;
+val_t s=board[P];
+return ((diagL[s+P]))+((diagR[s+(N-P)]))-2;}
 
-}
+ val_t qccount2(u32 P,u32 P2){
+//cannot be zero due being set from q[]
+val_t s=board[P],s2=board[P2];
+return ((diagL[s+P]))+((diagR[s+(N-P)]))+ ((diagL[s2+P2]))+((diagR[s2+(N-P2)]))-4;}
 
-size_t fstcols(){
+val_t fstcols(){
 for(size_t i=0;i<N;i++){if(qccount(i))return i;};return N;}
 
 //--------------------------
-void printboard(){print("\n");for(size_t i=0;i<N;i++)printf("%d\n",board[i]+1);}
+void printboard(){print("\n");for(size_t i=0;i<N;i++)print(board[i]+1,"\n");}
 //https://lemire.me/blog/2016/06/27/a-fast-alternative-to-the-modulo-reduction/
 
- uint32_t modreduce(uint32_t x, uint32_t N) {
+ val_t modreduce(uint32_t x, uint32_t N) {
   return ((uint64_t) x * (uint64_t) N) >> 32 ;
 }
-uint32_t rndcell(){return modreduce(randuint32(),N);}
+val_t rndcell(){return modreduce(randuint32(),N);}
 //-----------linear collission count----------
 #define countudiag() (sumL+sumR)
 //------------------------
@@ -75,7 +94,7 @@ uint32_t rndcell(){return modreduce(randuint32(),N);}
   print("\nT:",Ntime,"ms Col%",100.0*(N-cur)/N,"Swapt",swapt,"Valid%",100.0*swapt/tswaps);cend=__rdtsc();}}
 //--------------------------------------------
 void linearsolve(){
- A=0,B=0,valr=0;int minsearch=N>1024?log2index(N):0;
+ A=0,B=0,valr=0;val_t minsearch=N>1024?log2index(N):0;
 cend=__rdtsc();
  cur=countudiag(),best=cur;
 #if QDEBUG
@@ -85,11 +104,13 @@ loop:;
 #if QDEBUG
 loops++;
 #endif
-valr=randuint64();
-A=modreduce((valr>>32),N);
-B=modreduce(valr&0xffffffff,N);
-//if(0==(qccount(B)+qccount(A))||A==B)goto loop;
-if( !(qccount(B)+qccount(A)) ||A==B)goto loop;
+A=modreduce(rndgen32(),N);
+B=modreduce(rndgen32(),N);
+//valr=rndgen64();
+//A=modreduce((valr>>32),N);
+//B=modreduce(valr&0xffffffff,N);
+if(!qccount2(A,B)||A==B)goto loop;
+
 #if QDEBUG
 dir=1;
 #endif
@@ -119,7 +140,8 @@ print("\nEnd search:",mstime()," ms Collisions:",best);
 innerloop:;
 B=fstcols();
 innerloop2:;
-valr=randuint64();A=modreduce((valr>>32),N);
+A=modreduce(rndgen32(),N);
+//valr=rndgen64();A=modreduce((valr>>32),N);
 
 #if QDEBUG
 dir=1;
@@ -142,7 +164,7 @@ swapc(A,B);goto innerloop2;
 if(cur>0){goto loop;;}
 endl:; //end loop
 #if QDEBUG
- print("\nSolved N=",N," at:",mstime(),"ms Swaps:",swapt,"Fails:",tfail,"\n");
+ print("\nSolved N=",N," at:",mstime(),"ms Swaps:",swapt,"Fails:",tfail,"\n",tseedw);
 #endif
 }
 //===================Solver===========================
@@ -160,18 +182,18 @@ linearsolve();}
 int main(int argc,char**argv){
 if(argc<2){syntax:;puts("Syntax:nq N [p]\n N=Board size min=8 \n p=printboard");exit(1);}
  N=atoi(argv[1]);if(N<8)goto syntax;
-board=malloc(sizeof(int)*N);//queen row/cols(2^31-1 max)
+board=malloc(sizeof(val_t)*N);//queen row/cols(2^31-1 max)
 
 if(!board){perror("Queen array size too large for malloc");exit(2);}
-diagL=malloc(sizeof(int)*(N+2)*2);
-diagR=malloc(sizeof(int)*(N+2)*2);
+diagL=malloc(sizeof(val_t)*(N+2)*2);
+diagR=malloc(sizeof(val_t)*(N+2)*2);
 if(!diagR||!diagL){perror("Diag arrays size too large for malloc");exit(3);}
 for(size_t i=0;i<N;i++)board[i]=i;//unique rows/cols to swap.
 #if SCRAMBLE //speedup board solutions
 u64 timeseed=1111;
 setrseed(timeseed,1-~timeseed,timeseed-~timeseed,~timeseed+timeseed);
-for(int z=0;z<SCRAMBLE;z++){
-for(size_t i=0;i<N;i++){
+for(val_t z=0;z<SCRAMBLE;z++){
+for(val_t i=0;i<N;i++){
 u64 valr=randuint64();
 A=(valr>>32);B=valr&0xffffffff;
 A=modreduce(A,N);B=modreduce(B,N);
