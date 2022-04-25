a2b26f9b9612f02cc3d488dc643a83ae4fe9a0a6
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-04-25 08:43:23 +0300

    Update hashmix4.c

--- functions/hashing/hashmix4.c
+++ functions/hashing/hashmix4.c
@@ -3,7 +3,7 @@ static inline u64 rotate64(u64 x,u32 bits){return (x>>bits)|(x<<(64-bits));}
 u64 hashaddrt2(const u8* data,const size_t len){
 //add with rotate2
 u64 res=0x1234567812345178;
-const u64* data8=(u64*)&data;
+const u64* data8=(u64*)data;
 const size_t data8len=len/8;
 for(size_t i=0;i<data8len;i++){res+=rotate64(data8[i],res&63);}
 u64 rest[1]={0};
@@ -14,7 +14,7 @@ return res;}
 u64 hashaddrt(const u8* data,const size_t len){
 //add with rotate
 u64 res=0;u64 last=0x1234567812345678;
-const u64* data8=(u64*)&data;
+const u64* data8=(u64*)data;
 for(size_t i=0;i<(len/8);i++){
 res+=last;res+=rotate64(data8[i],res&63);last=~data8[i];}
 u64 rest[1]={0};
@@ -26,7 +26,7 @@ return res;}
 u64 hashxoraddrt(const u8* data,const size_t len){
 //xor add with rotate
 u64 res=0;u64 last=0x1234567812345678;
-const u64* data8=(u64*)&data;
+const u64* data8=(u64*)data;
 for(size_t i=0;i<(len/8);i++){
 res+=last;res^=data8[i];last= rotate64(data8[i],res&31);}
 for(size_t i=(len/8)<<3;i<len;i++){
@@ -37,7 +37,7 @@ return res;}
 u64 hashxoradd(const u8* data,const size_t len){
 //faster xor-add 
 u64 res=0;u64 last=0x1234567812345678;
-const u64* data8=(u64*)&data;
+const u64* data8=(u64*)data;
 for(size_t i=0;i<(len/8);i++){
 res^=data8[i];res+=last;last=~data8[i];}
 for(size_t i=(len/8)<<3;i<len;i++){
@@ -47,7 +47,7 @@ return res;}
 u64 hashmix4(const u8* data,const size_t len){
 //quick xor-add hash, untested
 u64 res=0;u64 mix[4]={0x1234567812345678,0x2345678123456781,0x3456781234567812,0x4567812345678123};
-const u64* data8=(u64*)&data;
+const u64* data8=(u64*)data;
 for(size_t i=0;i<(len/8);i++){
 res^=data8[i];res+=mix[i&3];mix[i&3]=data8[i];}
 for(size_t i=(len/8)<<3;i<len;i++){
