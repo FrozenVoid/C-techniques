7e3b3febaf46a56fc757095326f5fda2ba124e11
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-04-25 08:04:24 +0300

    Update hashmix4.c

--- functions/hashing/hashmix4.c
+++ functions/hashing/hashmix4.c
@@ -1,5 +1,16 @@
 static inline u64 rotate64(u64 x,u32 bits){return (x>>bits)|(x<<(64-bits));}
 
+u64 hashaddrt2(const u8* data,const size_t len){
+//add with rotate2
+u64 res=0x1234567812345178;
+const u64* data8=(u64*)&data;
+const size_t data8len=len/8;
+for(size_t i=0;i<data8len;i++){res+=rotate64(data8[i],res&63);}
+u64 rest[1]={0};
+memcpy(&rest[0],&data[(data8len)<<3],len-((data8len)<<3));
+res+=rotate64(rest[0],res&63);
+return res;}
+
 u64 hashaddrt(const u8* data,const size_t len){
 //add with rotate
 u64 res=0;u64 last=0x1234567812345678;
