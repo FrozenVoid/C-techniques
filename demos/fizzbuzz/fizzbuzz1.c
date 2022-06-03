211ec94b061bf72431df395e1cab670b0850fd22
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-03 07:59:35 +0300

    Create fizzbuzz1.c

---
+++ demos/fizzbuzz/fizzbuzz1.c
@@ -0,0 +1,7 @@
+puts(char*);sprintf(char*,char*,...);
+fris(bee){long long  fizzbuzz=8825495348169042246;
+if(bee%3)fizzbuzz>>=64>>!(bee%5);
+if(bee%5)fizzbuzz=(int)fizzbuzz;
+puts((fizzbuzz?:sprintf(&fizzbuzz,"%d\0",bee),&fizzbuzz));
+return -~bee;}
+main(bee){while(bee=fris(bee));}
