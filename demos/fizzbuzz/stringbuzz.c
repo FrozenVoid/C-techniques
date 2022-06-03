851888e218d94ccddd5b1d498de732503def877c
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-03 14:43:47 +0300

    Update stringbuzz.c

--- demos/fizzbuzz/stringbuzz.c
+++ demos/fizzbuzz/stringbuzz.c
@@ -1,7 +1,5 @@
-#include "stdio.h"
-    //FizzBuzz without variables,printf,separate strings,if/ternary/branches(except loop) or modulus/division/multiply use
-    int main(){
-
+//FizzBuzz without variables,printf,separate strings,if/ternary/branches(except loop) or modulus/division/multiply use
+main(){
     char
            //0  1 2 3 4 5 6
      num[]={48,48,0,1,1,1,58,'F','i','z','z',0,'B','u','z','z',0,0,0,'F','i','z','z','B','u','z','z',0};
@@ -12,6 +10,4 @@
     num[0]+=(num[1]==num[6]);
     num[1]>>=((num[1]==num[6]))<<3;
     num[1]+=(((!num[1])<<6)-((!num[1])<<4));
-    puts(num+(((!num[3])<<3)-!num[3])+((!num[5]<<2)+((!num[5])<<3)));
-
-    ;}}
+    puts(num+(((!num[3])<<3)-!num[3])+((!num[5]<<2)+((!num[5])<<3)));}}
