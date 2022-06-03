d8017e13756d7d2d5b44ff8581751c9de2bd74cf
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-03 13:22:53 +0300

    Update hexbuzz.c

--- demos/fizzbuzz/hexbuzz.c
+++ demos/fizzbuzz/hexbuzz.c
@@ -1,7 +1,7 @@
 puts(char*);putchar(c);
 #define twin(bee) ({fizzbuzz=bee;do{\
 putchar("0123456789ABCDEF"[fizzbuzz>>60]);}while(fizzbuzz<<=4);\
-putchar(bee&15?' ':'0');putchar('\x0a');})
+bee&15?:putchar('0');putchar('\x0a');})
 #define fris(bee) ({unsigned long long  fizzbuzz=8825495348169042246;\
 if(bee%3)fizzbuzz>>=64>>!(bee%5);\
 if(bee%5)fizzbuzz=(int)fizzbuzz;\
