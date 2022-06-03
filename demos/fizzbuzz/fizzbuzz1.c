56aeb06270cb65d14e62fee79ed050f6513a423d
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-03 14:54:50 +0300

    Update fizzbuzz1.c

--- demos/fizzbuzz/fizzbuzz1.c
+++ demos/fizzbuzz/fizzbuzz1.c
@@ -1,6 +1,6 @@
 #define fris(bee) ({long long  fizzbuzz=8825495348169042246;\
-if(bee%3)fizzbuzz>>=64>>!(bee%5);\
 if(bee%5)fizzbuzz=(int)fizzbuzz;\
+if(bee%3)fizzbuzz>>=32;\
 puts((fizzbuzz?:sprintf(&fizzbuzz,"%d\0",bee),&fizzbuzz));-~bee;})
 main(bee){while(bee=fris(bee));}
 
