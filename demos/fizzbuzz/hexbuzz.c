4d4fb07bba63e00327dc29f092755958fe18500f
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-03 13:31:56 +0300

    remove puts

--- demos/fizzbuzz/hexbuzz.c
+++ demos/fizzbuzz/hexbuzz.c
@@ -1,9 +1,10 @@
-puts(char*);putchar(c);
+putchar(c);
+#define buzzing(bee) ({do{putchar(fizzbuzz);}while(fizzbuzz>>=8);putchar('\x0a');})
 #define twin(bee) ({fizzbuzz=bee;do{\
 putchar("0123456789ABCDEF"[fizzbuzz>>60]);}while(fizzbuzz<<=4);\
 bee&15?:putchar('0');putchar('\x0a');})
 #define fris(bee) ({unsigned long long  fizzbuzz=8825495348169042246;\
 if(bee%3)fizzbuzz>>=64>>!(bee%5);\
 if(bee%5)fizzbuzz=(int)fizzbuzz;\
-fizzbuzz?puts(&fizzbuzz):twin(bee);-~bee;})
+fizzbuzz?buzzing(bee):twin(bee);-~bee;})
 main(bee){while(bee=fris(bee));}
