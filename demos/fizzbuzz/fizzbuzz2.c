efd67345f1bce2a987c6b05185baaa97d501a5ba
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-03 14:54:27 +0300

    Update fizzbuzz2.c

--- demos/fizzbuzz/fizzbuzz2.c
+++ demos/fizzbuzz/fizzbuzz2.c
@@ -4,7 +4,7 @@ do{putchar(bees);}while(fizzbuzz/=bzz);putchar('\x0a');})
 #define twin(bee) buzz(10,'0'+(bee/fizzbuzz)%10)
 
 #define fris(bee) ({unsigned long long  fizzbuzz=8825495348169042246;\
-if(bee%3)fizzbuzz>>=64>>!(bee%5);\
 if(bee%5)fizzbuzz=(int)fizzbuzz;\
+if(bee%3)fizzbuzz>>=32;\
 fizzbuzz?buzzing(bee):twin(bee);-~bee;})
 main(bee){while(bee=fris(bee));}
