64b914ed0ea6e60c30d70d5c2b22acfc907ef543
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-07-23 20:59:59 +0300

    Update marquee.c

--- Unix/text/effects/marquee.c
+++ Unix/text/effects/marquee.c
@@ -4,8 +4,13 @@
 #include <unistd.h>
 //see https://www.w3docs.com/learn-html/html-marquee-tag.html
 int main(int argc,char**argv){
-if(argc<2){printf("Syntax:marquee \"text message\"\n");return 1;}
-
+if(argc<2){printf("Syntax:marque \"text message\" [delay ms]\n");return 1;}
+int delayval=104166;
+if(argc==3){delayval=atoi(argv[2])*1000;}
 int maxlen=strlen(argv[1]),c=0;
-for(;;){printf("%s %.*s\r",&argv[1][c],c,argv[1]);c=c>=maxlen?0:c+1;fflush(stdout);usleep(104166);}
+for(;;){
+
+printf("%s %.*s\r",&argv[1][c],c,argv[1]);c=c>=maxlen?0:c+1;
+
+fflush(stdout);usleep(delayval);}
 }
